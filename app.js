fcfe74021d918fc76122fb1d9ec2aa73c73321c7
PedroCantanhede <pedroh.cantanhede@hotmail.com>
2021-04-09 21:57:37 -0300

    :beetle: Atualizações e Correções de Bugs

--- app.js
+++ app.js
@@ -5,4 +5,16 @@ $(document).ready(function(){
         $('.navbar').toggleClass('nav-toggle');
     });
 
+    $(window).on('scroll load',function(){
+
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+  
+        if($(window).scrollTop() > 0){
+          $('.scrollup').show();
+        }else{
+          $('.scrollup').hide();
+        }
+    });
+
 });
